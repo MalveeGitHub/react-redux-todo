cce55b738e86da3ad39a1f4fb102d204f0a67322
alvee <you@example.com>
2018-10-20 02:49:32 -0400

    Updates

---
+++ precache-manifest.81400b8bdd42afd7f88bbdacfe8716c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3474ac909b7cdc4f6b8f",
+    "url": "/react-redux-todo/static/js/runtime~main.3474ac90.js"
+  },
+  {
+    "revision": "91aaf8826208092f7900",
+    "url": "/react-redux-todo/static/js/main.91aaf882.chunk.js"
+  },
+  {
+    "revision": "9b098de170c97efaa708",
+    "url": "/react-redux-todo/static/js/1.9b098de1.chunk.js"
+  },
+  {
+    "revision": "91aaf8826208092f7900",
+    "url": "/react-redux-todo/static/css/main.0c7caf6e.chunk.css"
+  },
+  {
+    "revision": "6019564a2caf97d1e1f4d7bd87a1fc09",
+    "url": "/react-redux-todo/index.html"
+  }
+];
